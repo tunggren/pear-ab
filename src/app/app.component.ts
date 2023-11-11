98fc390db03aace3aff035889589c37510b2f1e0
Aron Ljunggren <aron.ljunggren@outlook.com>
2023-11-12 00:10:13 +0100

    deleted some weird stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,4 @@
-import { Component, Inject } from '@angular/core';
-import { Task } from './task/task';
-import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
-import { MatDialog } from '@angular/material/dialog';
-import { TaskDialogComponent, TaskDialogResult } from './task-dialog/task-dialog.component';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
-import { BehaviorSubject } from 'rxjs';
-
-const getObservable = (collection: AngularFirestoreCollection<Task>) => {
-  const subject = new BehaviorSubject<Task[]>([]);
-  collection.valueChanges({ idField: 'id' }).subscribe((val: Task[]) => {
-    subject.next(val);
-  });
-  return subject;
-};
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
@@ -23,67 +8,6 @@ const getObservable = (collection: AngularFirestoreCollection<Task>) => {
 
 export class AppComponent {
 
-  constructor(private dialog: MatDialog, @Inject(AngularFirestore) private store: AngularFirestore) {}
-
   title = 'pearAB';
-
-  todo = getObservable(this.store.collection('todo')) as Observable<Task[]>;
-  inProgress = getObservable(this.store.collection('inProgress')) as Observable<Task[]>;
-  done = getObservable(this.store.collection('done')) as Observable<Task[]>;
-
-  drop(event: CdkDragDrop<Task[]>): void {
-    if (event.previousContainer === event.container) {
-      return;
-    }
-    const item = event.previousContainer.data[event.previousIndex];
-    this.store.firestore.runTransaction(() => {
-      const promise = Promise.all([
-        this.store.collection(event.previousContainer.id).doc(item.id).delete(),
-        this.store.collection(event.container.id).add(item),
-      ]);
-      return promise;
-    });
-    transferArrayItem(
-      event.previousContainer.data,
-      event.container.data,
-      event.previousIndex,
-      event.currentIndex
-    );
-  }
-
-  newTask(): void {
-    const dialogRef = this.dialog.open(TaskDialogComponent, {
-      width: '270px',
-      data: {
-        task: {},
-      },
-    });
-    dialogRef
-      .afterClosed()
-      .subscribe((result: TaskDialogResult|undefined) => {
-        if (!result) {
-          return;
-        }
-      });
-  }
-
-  editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
-    const dialogRef = this.dialog.open(TaskDialogComponent, {
-      width: '270px',
-      data: {
-        task,
-        enableDelete: true,
-      },
-    });
-    dialogRef.afterClosed().subscribe((result: TaskDialogResult|undefined) => {
-      if (!result) {
-        return;
-      }
-      if (result.delete) {
-        this.store.collection(list).doc(task.id).delete();
-      } else {
-        this.store.collection(list).doc(task.id).update(task);
-      }
-    });
-  } 
+ 
 } 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { TaskComponent } from './task/task.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import { TaskDialogComponent } from './task-dialog/task-dialog.component';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
-import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent,
-    TaskDialogComponent,
-    
   ],
   imports: [
     BrowserModule,
@@ -40,16 +34,7 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
     MatInputModule,
     FormsModule,
     MatFormFieldModule,
-    provideFirebaseApp(() => initializeApp({        
-    apiKey: "AIzaSyClh2qc4-3Ssgb0eKO4kvHjmDXM_15Z0-k",
-    authDomain: "pear-ab-58438.firebaseapp.com",
-    databaseURL: "https://pear-ab-58438-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "pear-ab-58438",
-    storageBucket: "pear-ab-58438.appspot.com",
-    messagingSenderId: "779335499019",
-    appId: "1:779335499019:web:46368f1368ae0b1e15360e",
-    measurementId: "G-QPGYLS36NF" })),
-    provideFirestore(() => getFirestore()),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
